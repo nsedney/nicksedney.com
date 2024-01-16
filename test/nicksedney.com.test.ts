b31772065fb33323c1b941eb14ed131c852c647b
Nicholas Sedney <nsedney@gmail.com>
2024-01-16 13:17:25 -0800

    Init CDK project

---
+++ test/nicksedney.com.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NicksedneyCom from '../lib/nicksedney.com-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/nicksedney.com-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NicksedneyCom.NicksedneyComStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
