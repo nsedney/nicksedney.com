971dc07796feda0ead28f76902e6ee7a96009545
Nicholas Sedney <nsedney@gmail.com>
2024-01-17 15:42:54 -0800

    Add CDK config to configure and deploy website content entirely from this package.

--- lib/nicksedney.com-stack.ts
+++ lib/nicksedney.com-stack.ts
@@ -1,25 +1,37 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deployment from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 
 export class NicksedneyComStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, { ...props, analyticsReporting: false });
 
+    const accessLogsBucket = new s3.Bucket(this, 'nicksedneyAccessLogsBucket', {
+      bucketName: "nicksedney.com-access-logs",
+      objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+      lifecycleRules: [
+        { expiration: cdk.Duration.days(14) }
+      ],
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+
     const nicksedneyBucket = new s3.Bucket(this, "nicksedneybucket", {
       bucketName: "nicksedney.com",
       websiteIndexDocument: "index.html",
       websiteErrorDocument: "error.html",
-      blockPublicAccess: {
-        blockPublicAcls: true,
-        blockPublicPolicy: false,
-        ignorePublicAcls: true,
-        restrictPublicBuckets: false
-      },
+      serverAccessLogsBucket: accessLogsBucket,
+      serverAccessLogsPrefix: 'nicksedney.com/',
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
       publicReadAccess: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true
     });
 
+    const deployment = new s3deployment.BucketDeployment(this, "nicksedneyDeployWebsite", {
+      sources: [s3deployment.Source.asset("website")],
+      destinationBucket: nicksedneyBucket
+    });
   }
 }
