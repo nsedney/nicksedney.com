cab4e5276805f70800225ab7c7f127c13097210f
Nicholas Sedney <nsedney@gmail.com>
2024-01-18 18:55:57 -0800

    Add new stack for creating ssl cert in required region

--- bin/nicksedney.com.ts
+++ bin/nicksedney.com.ts
@@ -2,8 +2,19 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { NicksedneyComStack } from '../lib/nicksedney.com-stack';
+import { NicksedneyCertStack } from '../lib/nicksedneycertstack';
+import { RegionInfo } from 'aws-cdk-lib/region-info';
 
 const app = new cdk.App();
+
+// We need to create our HTTPS cert in a separate stack as it needs to exist in `us-east-1` region
+const certStack = new NicksedneyCertStack(app, 'NicksedneyCertStack', {
+  env: {
+    region: "us-east-1", // TODO: Are there defined types we could/should use?  Whats ts convention for constants?
+  },
+  crossRegionReferences: true,
+});
+
 new NicksedneyComStack(app, 'NicksedneyComStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
@@ -12,10 +23,4 @@ new NicksedneyComStack(app, 'NicksedneyComStack', {
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
---
+++ lib/nicksedneycertstack.ts
@@ -0,0 +1,25 @@
+import * as cdk from 'aws-cdk-lib';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import { Construct } from 'constructs';
+
+/*
+Stack explicitely for creating certificates.
+
+For certain resources to work (at least CloudFront), we must create our certs in region `us-east-1`; given
+we're operatin in a different region a separate stack is needed:
+https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_certificatemanager-readme.html#cross-region-certificates 
+*/
+export class NicksedneyCertStack extends cdk.Stack {
+    public readonly nickSedneyCert: acm.Certificate;
+
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, { ...props, analyticsReporting: false });
+
+        this.nickSedneyCert = new acm.Certificate(this, 'nicksedneyCertificate', {
+            domainName: 'nicksedney.com',
+            subjectAlternativeNames: ['*.nicksedney.com'],
+            certificateName: 'Nick Sedney Homepage',
+            validation: acm.CertificateValidation.fromDns()
+        });
+    }
+}
