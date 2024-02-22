13e6a48394f9d136fd36e54de1f0353fe2bf28e1
Nicholas Sedney <nsedney@gmail.com>
2024-02-22 12:58:23 -0800

    Set up forwarding to very site with bridgy as a fediverse handle.

--- lib/public-certificate-stack.ts
+++ lib/public-certificate-stack.ts
@@ -22,16 +22,20 @@ export interface PublicCertificatStackProps extends cdk.StackProps {
  * @remarks
  * For certain resources to work (at least CloudFront), we must create our certs in region `us-east-1`.
  * If we're operating in a different region, a separate stack is needed to deploy certificate where needed:
- * https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_certificatemanager-readme.html#cross-region-certificates 
+ * @see [cross-region-certificates](https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_certificatemanager-readme.html#cross-region-certificates )
  *  
- * @remarks
  * All domain names specified here must have a HostedZone allready created in Route53; otherwise deployment will hang
- * until a zone with appropriate CNAME record is added manually:
- * https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_certificatemanager-readme.html#dns-validation
- * This should be done automatically when registering a domain name.
+ * until a zone with appropriate CNAME record is added manually.
+ * This *should* be done automatically when registering a domain name.
+ * @see [dns-validation](https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_certificatemanager-readme.html#dns-validation)
+ * 
+ * A certificate will generally need to be re-generated on change.  This can get your stacks into a bad state as
+ * this stack will attempt to delete the old cert, before any consuming stacks have had a chance to update.
+ * So, any certificat updates will likely need to be a multi-step process:
+ * @see [unblock-cross-stack-references](https://www.endoflineblog.com/cdk-tips-03-how-to-unblock-cross-stack-references)
 */
 export class PublicCertificateStack extends cdk.Stack {
-    public readonly nickSedneyCert: acm.Certificate;
+    public readonly cert: acm.Certificate;
 
     constructor(scope: Construct, id: string, props: PublicCertificatStackProps) {
         super(scope, id, { ...props })
@@ -55,7 +59,7 @@ export class PublicCertificateStack extends cdk.Stack {
             }, {}
         )
 
-        this.nickSedneyCert = new acm.Certificate(this,
+        this.cert = new acm.Certificate(this,
             props.certificateResourceId ?? `${props.certificateResourceId ?? props.certificateDomain.registeredDomain + "Certificate"}`, {
             domainName: props.certificateDomain.registeredDomain,
             subjectAlternativeNames: alternateDomainsNames,
